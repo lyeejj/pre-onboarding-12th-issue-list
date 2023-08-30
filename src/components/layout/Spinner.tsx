af8f53d49bef30f72cadba31bf28d61b05ae5d6c
lyeejj <lyshine012@gmail.com>
2023-08-30 14:19:48 +0900

    feat: Spinner 컴포넌트 생성 및 스타일 적용

---
+++ src/components/layout/Spinner.tsx
@@ -0,0 +1,11 @@
+import '../../utils/styles/spinner.scss';
+
+function Spinner() {
+	return (
+		<div className="spinnerOverlay">
+			<div className="spinner"></div>
+		</div>
+	);
+}
+
+export default Spinner;
