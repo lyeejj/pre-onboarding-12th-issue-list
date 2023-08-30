762129a64209779c32090b523374d151c0924433
lyeejj <lyshine012@gmail.com>
2023-08-30 13:19:06 +0900

    feat: Layout 컴포넌트 구현

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { API_INFO } from '../../utils/constants';
 
 type LayoutProps = {
 	children: React.ReactNode;
 };
 
 function Layout({ children }: LayoutProps) {
-	return <></>;
+	const OWNER = API_INFO.OWNER;
+	const REPO = API_INFO.REPO;
+	return (
+		<div>
+			<header>
+				{OWNER} / {REPO}
+			</header>
+			<main>{children}</main>
+		</div>
+	);
 }
 export default Layout;
