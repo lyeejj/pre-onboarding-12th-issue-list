cdea9d883e2c316bae4fa73bcbb26ad9e292b074
lyeejj <lyshine012@gmail.com>
2023-08-30 14:20:53 +0900

    feat: Router, Suspense, lazy 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-	return <>초기세팅</>;
-}
+import { Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import Layout from './components/layout/Layout';
+import Spinner from './components/layout/Spinner';
 
+const IssueList = lazy(() => import('./pages/IssueList'));
+const IssueDetail = lazy(() => import('./pages/IssueDetail'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+
+function App() {
+	return (
+		<Layout>
+			<Suspense fallback={<Spinner />}>
+				<Routes>
+					<Route path="/" element={<IssueList />} />
+					<Route path="/issue/:issue_number" element={<IssueDetail />} />
+					<Route path="*" element={<NotFoundPage />} />
+				</Routes>
+			</Suspense>
+		</Layout>
+	);
+}
 export default App;
